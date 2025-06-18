44becda4c99c0826cedc414bece6d9d24b68ff5e
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-06-18 10:51:43 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      
       <Text>Opá, tudo bem?</Text>
       <StatusBar style="auto" />
     </View>
