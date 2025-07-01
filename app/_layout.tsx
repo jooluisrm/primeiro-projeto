d3d46be125d7052ec93623c5315b479d103cbf8e
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-07-01 14:53:18 -0300

    Configurações no Stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,16 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
     return (
-        <Stack>
-            <Stack.Screen name="index" options={{title: "Home", headerShown: false}} />
+        <Stack screenOptions={{
+            headerStyle: {
+                backgroundColor: "#ff0000"
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+                fontWeight: "bold"
+            }
+        }}>
+            <Stack.Screen name="index" options={{title: "Home"}} />
             <Stack.Screen name="about" options={{title: "Sobre"}} />
         </Stack>
     )
--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,19 @@
-import { Link, router } from "expo-router";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Link, router, Stack, useNavigation } from "expo-router";
+import { Button, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Screen() {
 
     const handleClick = () => {
-        router.replace("/about");
+        router.navigate("/about");
     }
 
     return (
         <View style={styles.container}>
+            <Stack.Screen options={{
+                title: "Início",
+                headerRight: () => <Button title="Adicionar"/>
+            }} />
+
             <Text>Opa, tudo bem?</Text>
 
             <Pressable onPress={handleClick} style={styles.button}>
