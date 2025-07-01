0dc1b0bc1fc0350091cd075b8101befb3aae169c
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-07-01 09:55:56 -0300

    Estilizando botões com Pressable

--- App.tsx
+++ App.tsx
@@ -1,21 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      
-      <Text>Opá, tudo bem?</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const handleClick = () => {
+        alert("Funcionou!!!");
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Opá, tudo bem?</Text>
+
+            <Pressable style={styles.btn} onPress={handleClick}>
+                <Text style={styles.btnText}>Clique aqui</Text>
+            </Pressable>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    btn: {
+        width: 200,
+        height: 40,
+        backgroundColor: "#0000FF",
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    btnText: {
+        color: "#ffffff",
+        fontWeight: "bold",
+        fontSize: 16
+    }
 });
